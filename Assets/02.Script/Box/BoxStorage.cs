0efd531dfa971a2e728d5ebff7f43b156c1a0f56
Dev-Lee-Jae <jlky1@naver.com>
2024-11-16 22:36:01 +0900

    feat: box storage
    - Box를 추가 할 수 있습니다.
    - Box를 빼게 되면 박스들이 정렬된 위치에 맞게 이동합니다.

--- Assets/02.Script/Box/BoxStorage.cs
+++ Assets/02.Script/Box/BoxStorage.cs
@@ -1,5 +1,7 @@
+using EverythingStore.Optimization;
 using Sirenix.OdinInspector;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -20,6 +22,11 @@ namespace EverythingStore.InteractionObject
 		[SerializeField] private int _capacity;
 		[SerializeField] private BoxStoragePointData _pivotData;
 		private Queue<Box> _boxQueue = new();
+
+		[Title("Box Move")]
+		[SerializeField] private float _boxMoveTime;
+
+		private IEnumerator _cBoxMove;
 		#endregion
 
 		#region Property
@@ -32,6 +39,16 @@ namespace EverythingStore.InteractionObject
 		#endregion
 
 		#region UnityCycle
+		private void Start()
+		{
+			ObjectPoolManger _poolManger = GameObject.FindObjectOfType<ObjectPoolManger>();
+
+			for (int i = 0; i < _capacity; i++)
+			{
+				Box box = _poolManger.GetPoolObject(PooledObjectType.Box_Normal).GetComponent<Box>();
+				AddBox(box);
+			}
+		}
 		private void OnDrawGizmos()
 		{
 			if (_gizomo == false)
@@ -43,13 +60,12 @@ namespace EverythingStore.InteractionObject
 			style.fontSize = _fontSize;
 
 			int count = 0;
-			foreach(var item in _pivotData.Points)
+			foreach (var item in _pivotData.Points)
 			{
 				Vector3 center = item + _pivot.position + Vector3.up * (_gizmoBoxSize * 0.5f);
-				Vector3 handlePoint = center + Vector3.right * _gizmoBoxSize;
 
-				Handles.Label(handlePoint, count.ToString(), style);
-				Gizmos.DrawCube(center, Vector3.one * _gizmoBoxSize);
+				Handles.Label(center, count.ToString(), style);
+				Gizmos.DrawWireCube(center, Vector3.one * _gizmoBoxSize);
 
 				count++;
 			}
@@ -62,9 +78,22 @@ namespace EverythingStore.InteractionObject
 		/// </summary>
 		public void AddBox(Box box)
 		{
+			var point = GetPivotPoint(_boxQueue.Count);
 			box.transform.parent = transform;
+			box.transform.localPosition = point;
 			_boxQueue.Enqueue(box);
 		}
+
+		[Button("Test")]
+		public Box RemoveBox()
+		{
+			var box = _boxQueue.Dequeue();
+			box.gameObject.SetActive(false);
+			BoxPointReset();
+			return box;
+		}
+
+
 
 		internal void SavePointData(List<Vector3> points)
 		{
@@ -73,6 +102,40 @@ namespace EverythingStore.InteractionObject
 		#endregion
 
 		#region Private Method
+		private Vector3 GetPivotPoint(int index)
+		{
+			return _pivotData.Points[index];
+		}
+
+		private void BoxPointReset()
+		{
+			if(_cBoxMove != null)
+			{
+				StopCoroutine(_cBoxMove);
+			}
+			_cBoxMove = C_BoxMove();
+			StartCoroutine(_cBoxMove);
+		}
+
+		private IEnumerator C_BoxMove()
+		{
+			float time = 0.0f;
+
+			while (time < _boxMoveTime)
+			{
+				float progress = time / _boxMoveTime;
+
+				int index = 0;
+				foreach (var item in _boxQueue)
+				{
+					Vector3 localPos = Vector3.Lerp(item.transform.localPosition, _pivotData.Points[index], progress);
+					item.transform.localPosition = localPos;
+					index++;
+				}
+				time += Time.deltaTime;
+				yield return null;
+			}
+		}
 		#endregion
 	}
 }
